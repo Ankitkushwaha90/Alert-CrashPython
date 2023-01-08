5ec6376a3e50cc9cdc67ead3461398991745659a
Docker <ankitkushwahahacker99109@gmail.com>
2023-01-08 11:30:14 +0530

    VirushCrushPython

---
+++ Virus!.py
@@ -0,0 +1,14 @@
+import os 
+j = 1
+
+for i in range(1, 5):
+	k = str(j)
+	olt = "you are hacked" + k
+	print(olt)
+	klto = str(olt)
+	#print(open.mkdir(klto))	
+	os.system("explorer")
+	os.startfile("cmd")
+	os.startfile("d:")
+	os.startfile("crome")
+	j += 1
